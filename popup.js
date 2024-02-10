873e9039f96ea2538c43602c0e508f664fa6adbf
mariaem21 <mematamoros@icloud.com>
2024-02-10 16:52:53 -0600

    Added other options for subjects

--- popup.js
+++ popup.js
@@ -17,13 +17,28 @@ function getRandomDogFact() {
         });
 }
 
-// function getTayTayLyric() {
-//     return fetch('https://taylor-swift-api.sarbo.workers.dev/lyrics?shouldRandomizeLyrics=true&numberOfParagraphs=1');
-// }
+function getTayTayLyric() {
+    return fetch('https://taylor-swift-api.sarbo.workers.dev/lyrics?shouldRandomizeLyrics=true&numberOfParagraphs=1')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch lyric.');
+            }
+            return response.json();
+        })
+        .then(lyrics => {
+            const fact = lyrics.lyrics[0];
+            console.log(lyrics);
+            return fact;
+        })
+        .catch(error => {
+            console.error('Error fetching Tay Tay lyric:', error);
+            return null;
+        })
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
     const dialogBox = document.getElementById('dialog-box');
-    const query = { active: true, currentWindow: true };
+    // const query = { active: true, currentWindow: true };
 
     try {
         const fact = await getRandomFunFact();
@@ -41,10 +56,13 @@ async function getRandomFunFact() {
 
     if (subject == "Dogs") {
         const fact = await getRandomDogFact();
-        text = `Fun fact about ${subject}. ${fact}`;
+        text = `Fun fact about ${subject}:\n ${fact}`;
         return text;
+    
     } else if (subject == "Taylor Swift") {
-        return `Here is a Taylor Swift song lyric:`;
+        const fact = await getTayTayLyric();
+        return `Here is a Taylor Swift song lyric:\n ${fact}`;
+    
     } else {
         return text;
     }
@@ -52,10 +70,11 @@ async function getRandomFunFact() {
 
 const subjects = [
     'Taylor Swift',
-    'the Chiefs',
-    'Dogs',
-    'UTSA',
-    'Engineering'
+    'Dogs'
+    // 'Dogs',
+    // 'the Chiefs',
+    // 'UTSA',
+    // 'Engineering'
 ]
 
 const getRandomSubject = () => {
